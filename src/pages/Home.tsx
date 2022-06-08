03f1a22ec3b71f43eb9bc29bcaaf1b0d5273319d
Gabriel C. Brandão <62272513+GabrielBrandao13@users.noreply.github.com>
2022-06-08 14:12:47 -0300

    Dependências necessárias para estilização adicionadas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
+import { FormEvent } from "react"
+
 function Home(){
+    function handleFormSubmit(e:FormEvent){
+
+    }
     return (
         <div>
             <h2>Home</h2>
+            <form onSubmit={handleFormSubmit}>
+                <label htmlFor="custom-name">Custom name</label>
+                <input type="text" name="custom-name" id="custom-name" />
+                <label htmlFor="url">URL</label>
+                <input type="text" name="url" id="url" />
+            </form>
         </div>
     )
 }
