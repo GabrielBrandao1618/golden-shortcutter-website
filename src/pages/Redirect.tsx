2bf201ad282f4ccbfb6b1ee7525c5a2a3ec4d521
Gabriel C. Brandão <62272513+GabrielBrandao13@users.noreply.github.com>
2022-06-09 18:01:18 -0300

    Criada resposta para caso o nome do link seja inválido

--- src/pages/Redirect.tsx
+++ src/pages/Redirect.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components'
+
 import { useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom'
 
@@ -11,14 +13,22 @@ type getUrlServerResponse = {
     ref: string;
     name: string;
 }
-
-function Redirect(){
+type props = {
+    className?:string;
+}
+function Redirect({className}:props){
     const {name} = useParams<Params>()
 
     const [url, setUrl] = useState('')
+    const [urlExists, setUrlExists] = useState(true)
 
     useEffect(() => {
-        api.get<getUrlServerResponse>(`/getUrl/${name}`).then(response => setUrl(response.data.ref))
+        api.get<getUrlServerResponse>(`/getUrl/${name}`).then(response => {
+            if(response.data.ref.trim() === ''){
+                setUrlExists(false)
+            }
+            setUrl(response.data.ref)
+        })
     }, [])
     useEffect(()=> {
         if(url.trim() !== ''){
@@ -26,11 +36,47 @@ function Redirect(){
         }
     }, [url])
     return (
-        <div>
-            <h2>Aguarde</h2>
-            <p>Em alguns instantes você será redirecionado para onde deseja</p>
+        <div className={className}>
+            <div className="menu">
+                <h1>{urlExists? 'Hold up!' : 'Oh...'}</h1>
+                {urlExists? (
+                    <p className="paragraph">You're being redirected to another website...</p>
+                ) : (
+                    <p className="error-text">The website you're trying to access doesn't exist...</p>
+                )}
+            </div>
         </div>
     )
 }
 
-export {Redirect}
+const StyledRedirect = styled(Redirect)`
+    width: 100vw;
+    height: 100vh;
+    display:flex;
+    flex-flow: column nowrap;
+    align-items:center;
+    justify-content:center;
+
+    .menu {
+        width:100%;
+        max-width: 400px;
+        background-color:#ffc400;
+        border-radius: 8px;
+
+        h1, p {
+            width: 100%;
+            text-align:center;
+        }
+        p {
+            font-size:12pt;
+        }
+        
+        p.error-text {
+            color:red;
+            font-weight: bold;
+            font-size:13pt;
+        }
+    }
+`
+
+export {StyledRedirect as Redirect}
