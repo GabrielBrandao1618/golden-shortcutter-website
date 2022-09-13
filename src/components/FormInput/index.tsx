b10d61276a3b6f85e83b13b17ab4fdb04e753c65
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-09-13 14:03:45 -0300

    Melhorados estilos dos erros dos inputs

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -1,13 +1,21 @@
+import styled from 'styled-components';
 import {Control, Controller} from 'react-hook-form'
 
 import { CustomInputProps, Input } from "../Input";
 
+
 import {FieldError} from 'react-hook-form'
 interface Props extends CustomInputProps {
     name: string;
-    control: Control;
+    control: any;
     error?: FieldError;
 }
+
+const Error = styled.span`
+    color: red;
+    font-size: 12pt;
+    font-weight: bold;
+`
 
 export function FormInput(props: Props){
     return (
@@ -22,7 +30,7 @@ export function FormInput(props: Props){
                             onChange={onChange}
                             {...props}
                         />
-                        <span>{props.error?.message}</span>
+                        <Error>{props.error?.message}</Error>
                     </>
                 )
             }}
